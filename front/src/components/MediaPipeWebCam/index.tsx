c01dba4bb8953e36be192f8bcec853bcaeaad210
Malza0408 <parkjh04080@gmail.com>
2022-06-24 19:24:43 +0900

    chore : learning mediapipe

--- front/src/components/MediaPipeWebCam/index.tsx
+++ front/src/components/MediaPipeWebCam/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import {
   Holistic,
   HAND_CONNECTIONS,
@@ -32,88 +32,120 @@ interface WebCamProps {
 function MediaPipeWebCam({ cameraOn }: WebCamProps) {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  let camera = null;
+  // let camera = null;
+  const [camera, setCamera] = useState<cam.Camera>();
+  const [test, setTest] = useState<h.NormalizedLandmarkList[]>([])
 
   const onResults: h.ResultsListener = (results) => {
-    if (!canvasRef.current || !webcamRef.current?.video) {
-      return;
+    // console.log(results.poseLandmarks);
+    if(test.length <= 30) {
+      setTest((cur):h.NormalizedLandmarkList[] => {
+        const temp = [...cur];
+        temp.push(results.poseLandmarks);
+        console.log(temp);
+        
+        return temp;
+      })
     }
+    
 
-    canvasRef.current.width = webcamRef.current?.video.videoWidth;
-    canvasRef.current.height = webcamRef.current?.video.videoHeight;
+    
+    // if (!canvasRef.current || !webcamRef.current?.video) {
+    //   return;
+    // }
 
-    const canvasElement = canvasRef.current;
-    const canvasCtx = canvasElement.getContext("2d");
+    // canvasRef.current.width = webcamRef.current?.video.videoWidth;
+    // canvasRef.current.height = webcamRef.current?.video.videoHeight;
 
-    if (!canvasCtx) {
-      return;
-    }
+    // const canvasElement = canvasRef.current;
+    // const canvasCtx = canvasElement.getContext("2d");
 
-    canvasCtx.save();
+    // if (!canvasCtx) {
+    //   return;
+    // }
 
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.globalCompositeOperation = "source-in";
-    canvasCtx.fillStyle = "#00FF00";
-    canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+    // canvasCtx.save();
 
-    canvasCtx.globalCompositeOperation = "destination-atop";
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
-    canvasCtx.globalCompositeOperation = "source-over";
+    // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    // canvasCtx.globalCompositeOperation = "source-in";
+    // canvasCtx.fillStyle = "#00FF00";
+    // canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
-    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-      color: "#00FF00",
-      lineWidth: 4,
-    });
-    drawLandmarks(canvasCtx, results.poseLandmarks, {
-      color: "#FF0000",
-      lineWidth: 2,
-    });
-    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {
-      color: "#CC0000",
-      lineWidth: 5,
-    });
-    drawLandmarks(canvasCtx, results.leftHandLandmarks, {
-      color: "#00FF00",
-      lineWidth: 2,
-    });
-    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
-      color: "#00CC00",
-      lineWidth: 5,
-    });
-    drawLandmarks(canvasCtx, results.rightHandLandmarks, {
-      color: "#FF0000",
-      lineWidth: 2,
-    });
+    // canvasCtx.globalCompositeOperation = "destination-atop";
+    // canvasCtx.drawImage(
+    //   results.image,
+    //   0,
+    //   0,
+    //   canvasElement.width,
+    //   canvasElement.height
+    // );
+    // canvasCtx.globalCompositeOperation = "source-over";
+
+    // drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+    //   color: "#00FF00",
+    //   lineWidth: 4,
+    // });
+    // drawLandmarks(canvasCtx, results.poseLandmarks, {
+    //   color: "#FF0000",
+    //   lineWidth: 2,
+    // });
+    // drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {
+    //   color: "#CC0000",
+    //   lineWidth: 5,
+    // });
+    // drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+    //   color: "#00FF00",
+    //   lineWidth: 2,
+    // });
+    // drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
+    //   color: "#00CC00",
+    //   lineWidth: 5,
+    // });
+    // drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+    //   color: "#FF0000",
+    //   lineWidth: 2,
+    // });
   };
 
   useEffect(() => {
-    if (cameraOn) {
-      holistic.onResults(onResults);
-      if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null
-      ) {
-        if (!webcamRef.current?.video) {
-          return;
-        }
-        camera = new cam.Camera(webcamRef.current?.video, {
-          onFrame: async () => {
-            if (!webcamRef.current?.video) {
-              return;
-            }
-            await holistic.send({ image: webcamRef.current?.video });
-          },
-          width: 640,
-          height: 480,
-        });
-        camera.start();
+    holistic.onResults(onResults);
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      if (!webcamRef.current?.video) {
+        return;
       }
+      const newCamera = new cam.Camera(webcamRef.current?.video, {
+        onFrame: async () => {
+          if (!webcamRef.current?.video) {
+            return;
+          }
+          await holistic.send({ image: webcamRef.current?.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      setCamera(newCamera);
+    }
+  }, []);
+
+  useEffect(() => {
+    const start = async () => {
+      await camera?.start();
+    }
+    const end = async() => {
+      await camera?.stop()
+    }
+
+    try {
+      start();
+
+      return () => {
+        end();
+      }
+    } catch(e: any) {
+      throw new Error(e)
     }
   }, [cameraOn]);
 
@@ -131,7 +163,7 @@ function MediaPipeWebCam({ cameraOn }: WebCamProps) {
           height: 480,
         }}
       />
-      {cameraOn && <UserCanvas ref={canvasRef} />}
+      {/* {cameraOn && <UserCanvas ref={canvasRef} />} */}
     </>
   );
 }
