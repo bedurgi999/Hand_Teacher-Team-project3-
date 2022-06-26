a9d63684db6c57b883c8bde141262ca1eb413f60
Hye-S <gptjs7970@naver.com>
2022-06-27 01:21:50 +0900

    refactor: model_load_HS, server, test file 코드 수정

--- AI/model/server_HS/server.py
+++ AI/model/server_HS/server.py
@@ -1,31 +1,14 @@
 from flask_socketio import SocketIO, emit, disconnect
 from flask import Flask
 from flask_cors import CORS
-from random import random
 from threading import Thread, Event
-from time import sleep
 
-from model_load_HS import prediction
+from model_load_HS import HandSignModel
 
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socket = SocketIO(app, cors_allowed_origins='*', engineio_logger=True)
-
-# prediction generator
-class HandSignModel:
-    def __init__(self, mode):
-        # self.delay = 0.5
-        # super(ModelPrediction, self).__init__()
-        self.mode = mode
-
-    def predict(self, data):
-        print('start prediction')
-        return prediction(data, mode=self.mode)
-
-    def run(self, data):
-        result = self.predict(data)
-        return result
 
 
 # Handle the webapp 
@@ -37,17 +20,16 @@ def connect_socket():
 
 @socket.on('coordinate')
 def handle_coordinate(data):
-    print('coordinate', data)
-    model = HandSignModel(mode='A')
-    result = model.run(data)
+    model = HandSignModel('A')
+    result = model.predict(data)
     emit("answer", result)
 
 
 @socket.on("disconnect")
-def disconnect_socket(payload):
-    emit("disconnect", "ok")
+def disconnect_socket():
+    # emit("disconnect", "ok")
     disconnect()
 
 
 if __name__ == '__main__':
-    socket.run(app, port = 4000)
+    socket.run(app, port=4000)
